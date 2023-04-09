5596250bc2333fe12ddae9e23da604170e0b04cb
Alex Goryanin <goryaninaa@icloud.com>
2023-04-09 21:47:30 +0400

    Add service util JUnit test

---
+++ src/test/java/com/goryaninaa/web/bank/service/util/RequisitesGenerator.java
@@ -0,0 +1,23 @@
+package com.goryaninaa.web.bank.service.util;
+
+import com.goryaninaa.web.bank.model.account.Account;
+import com.goryaninaa.web.bank.model.account.AccountOpenRequisites;
+import com.goryaninaa.web.bank.model.account.AccountType;
+import com.goryaninaa.web.bank.model.client.Client;
+import com.goryaninaa.web.bank.model.operation.OperationRequisites;
+import com.goryaninaa.web.bank.model.operation.ServiceInitiator;
+
+public class RequisitesGenerator {
+
+    public static OperationRequisites defineOperationRequisites() {
+        OperationRequisites requisites =
+                new OperationRequisites(5, ServiceInitiator.POSTMAN, new Client());
+        requisites.setAccountFrom(new Account(1));
+        requisites.setAccountRecipient(new Account(2));
+        return requisites;
+    }
+
+    public static AccountOpenRequisites defineOpenRequisites() {
+        return new AccountOpenRequisites(defineOperationRequisites(), AccountType.VKLAD75, 1);
+    }
+}
