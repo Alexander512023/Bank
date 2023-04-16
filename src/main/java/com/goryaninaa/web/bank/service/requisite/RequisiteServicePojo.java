11b34a427254ef749567cc2967d3a3140a08e7ef
Alex Goryanin <goryaninaa@icloud.com>
2023-04-16 19:31:40 +0400

    Fix checkstyle and PMD issues in requisite service package etc.

--- src/main/java/com/goryaninaa/web/bank/service/requisite/RequisiteServicePojo.java
+++ src/main/java/com/goryaninaa/web/bank/service/requisite/RequisiteServicePojo.java
@@ -12,45 +12,47 @@ import com.goryaninaa.web.bank.service.account.RequisiteServiceAccount;
 import com.goryaninaa.web.bank.service.operation.RequisiteServiceOperation;
 import java.util.Optional;
 
+/**
+ * This is simple implementation of {@link RequisiteServiceAccount} and
+ * {@link RequisiteServiceOperation} interfaces. It operates {@link ClientRepositoryRequisite} in
+ * order to get source of client's data.
+ */
 public class RequisiteServicePojo implements RequisiteServiceAccount, RequisiteServiceOperation {
 
   private static final String MESSAGE = "There is no such client";
   private final ClientRepositoryRequisite clientRepository;
 
-  public RequisiteServicePojo(ClientRepositoryRequisite clientRepository) {
+  public RequisiteServicePojo(final ClientRepositoryRequisite clientRepository) {
     this.clientRepository = clientRepository;
   }
 
   @Override
-  public AccountOpenRequisites prepareAccountOpenRequisites(AccountOpenRequisites requisites)
+  public AccountOpenRequisites prepareAccountOpenRequisites(final AccountOpenRequisites requisites)
       throws AccountOpenException {
     return enrichOpenAccountRequisites(requisites);
   }
 
   @Override
-  public OperationRequisites prepareAccountOpenOperationRequisites(Account account,
-                                                                   AccountOpenRequisites requisites)
-      throws AccountOpenException {
+  public OperationRequisites prepareAccountOpenOperationRequisites(
+      final Account account, final AccountOpenRequisites requisites) throws AccountOpenException {
     return prepareFirstOperationRequisites(requisites, account);
   }
 
   @Override
-  public OperationRequisites prepareDepositOperationRequisites(Account account,
-                                                               OperationRequisites requisites)
-      throws AccountDepositException {
+  public OperationRequisites prepareDepositOperationRequisites(
+      final Account account, final OperationRequisites requisites) throws AccountDepositException {
     return enrichDepositRequisites(requisites, account);
   }
 
   @Override
-  public OperationRequisites prepareWithdrawOperationRequisites(Account account,
-                                                                OperationRequisites requisites)
-      throws AccountWithdrawException {
+  public OperationRequisites prepareWithdrawOperationRequisites(
+      final Account account, final OperationRequisites requisites) throws AccountWithdrawException {
     return enrichWithdrawRequisites(requisites, account);
   }
 
-  private AccountOpenRequisites enrichOpenAccountRequisites(AccountOpenRequisites requisites)
+  private AccountOpenRequisites enrichOpenAccountRequisites(final AccountOpenRequisites requisites)
       throws AccountOpenException {
-    Optional<Client> initiator = clientRepository.findByPassport(
+    final Optional<Client> initiator = clientRepository.findByPassport(
         requisites.getOperRequisites().getClient().getPassport());
     if (initiator.isPresent()) {
       requisites.getOperRequisites().setClient(initiator.get());
@@ -60,22 +62,21 @@ public class RequisiteServicePojo implements RequisiteServiceAccount, RequisiteS
     }
   }
 
-  private OperationRequisites prepareFirstOperationRequisites(AccountOpenRequisites requisites,
-                                                              Account account)
-      throws AccountOpenException {
-    AccountOpenRequisites enrichedRequisites = enrichOpenAccountRequisites(requisites);
-    OperationRequisites firstOperationRequisites = enrichedRequisites.getOperRequisites();
-    firstOperationRequisites.setAccount(account);
-    firstOperationRequisites.setAccountRecipient(account);
-    firstOperationRequisites.setOperationType(OperationType.DEPOSIT);
-    firstOperationRequisites.setHistoryNumber(1);
-    return firstOperationRequisites;
+  private OperationRequisites prepareFirstOperationRequisites(
+      final AccountOpenRequisites requisites, final Account account) throws AccountOpenException {
+    final AccountOpenRequisites enrichedReq = enrichOpenAccountRequisites(requisites);
+    final OperationRequisites firstOperReq = enrichedReq.getOperRequisites();
+    firstOperReq.setAccount(account);
+    firstOperReq.setAccountRecipient(account);
+    firstOperReq.setOperationType(OperationType.DEPOSIT);
+    firstOperReq.setHistoryNumber(1);
+    return firstOperReq;
   }
 
-  private OperationRequisites enrichDepositRequisites(OperationRequisites requisites,
-                                                      Account account)
-      throws AccountDepositException {
-    Optional<Client> client = clientRepository.findByPassport(requisites.getClient().getPassport());
+  private OperationRequisites enrichDepositRequisites(
+      final OperationRequisites requisites, final Account account) throws AccountDepositException {
+    final Optional<Client> client = clientRepository
+        .findByPassport(requisites.getClient().getPassport());
     if (client.isPresent()) {
       requisites.enrich(account, client.get(), OperationType.DEPOSIT);
       return requisites;
@@ -84,10 +85,10 @@ public class RequisiteServicePojo implements RequisiteServiceAccount, RequisiteS
     }
   }
 
-  private OperationRequisites enrichWithdrawRequisites(OperationRequisites requisites,
-                                                       Account account)
-      throws AccountWithdrawException {
-    Optional<Client> client = clientRepository.findByPassport(requisites.getClient().getPassport());
+  private OperationRequisites enrichWithdrawRequisites(
+      final OperationRequisites requisites, final Account account) throws AccountWithdrawException {
+    final Optional<Client> client = clientRepository
+        .findByPassport(requisites.getClient().getPassport());
     if (client.isPresent()) {
       requisites.enrich(account, client.get(), OperationType.WITHDRAW);
       return requisites;
